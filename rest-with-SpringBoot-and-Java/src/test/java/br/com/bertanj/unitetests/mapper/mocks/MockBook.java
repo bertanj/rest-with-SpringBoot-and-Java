023e22f3fe501bd2108079f1c592a59fd9c767c7
Mateus Bertani Dias <mateusbertani@edu.unifil.br>
2025-09-30 18:15:22 -0300

    create books tests with mockito

---
+++ rest-with-SpringBoot-and-Java/src/test/java/br/com/bertanj/unitetests/mapper/mocks/MockBook.java
@@ -0,0 +1,59 @@
+package br.com.bertanj.unitetests.mapper.mocks;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+
+import br.com.bertanj.data.dto.v1.BookDTO;
+import br.com.bertanj.model.Book;
+
+public class MockBook {
+
+
+    public Book mockEntity() {
+        return mockEntity(0);
+    }
+    
+    public BookDTO mockDTO() {
+        return mockDTO(0);
+    }
+    
+    public List<Book> mockEntityList() {
+        List<Book> books = new ArrayList<Book>();
+        for (int i = 0; i < 14; i++) {
+            books.add(mockEntity(i));
+        }
+        return books;
+    }
+
+    public List<BookDTO> mockDTOList() {
+        List<BookDTO> books = new ArrayList<>();
+        for (int i = 0; i < 14; i++) {
+            books.add(mockDTO(i));
+        }
+        return books;
+    }
+    
+    public Book mockEntity(Integer number) {
+        Book book = new Book();
+        book.setId(number.longValue());
+        book.setAuthor("Some Author" + number);
+        book.setLaunchDate(new Date());
+        book.setPrice(new BigDecimal("25.0"));
+        book.setTitle("Some Title" + number);
+        return book;
+    }
+
+    public BookDTO mockDTO(Integer number) {
+        BookDTO book = new BookDTO();
+        book.setId(number.longValue());
+        book.setAuthor("Some Author" + number);
+        book.setLaunchDate(new Date());
+        book.setPrice(new BigDecimal("25.0"));
+        book.setTitle("Some Title" + number);
+        return book;
+    }
+
+}
